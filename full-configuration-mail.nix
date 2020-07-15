fb03bf4ff3faa199fb9ca16f78fa207236d26d43
Steve Sosik <ssosik@gmail.com>
2020-07-14 20:27:52 -0400

    adding event queue and service degraded check

--- full-configuration-mail.nix
+++ full-configuration-mail.nix
@@ -166,9 +166,13 @@ in
          set daemon 300 with start delay 120
          set mailserver mail.little-fluffy.cloud
          set alert root@little-fluffy.cloud
+         set eventqueue basedir /var/monit slots 5000
 
          check filesystem rootfs with path /dev/vda3
                 if space usage > 30% then alert
+
+         check program SystemDegraded with path "/run/current-system/sw/bin/systemctl is-system-running"
+           if status != 0 then alert
       '';
     };
 
