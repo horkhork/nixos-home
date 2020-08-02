2dc669098385aeefd5e864025984969b58adab3b
Steve Sosik <ssosik@gmail.com>
2020-08-02 16:18:59 -0400

    Updates

--- full-configuration-mail.nix
+++ full-configuration-mail.nix
@@ -131,6 +131,7 @@ in
     inetutils
     mkpasswd
     #mtr
+    openssl
     service-wrapper
     sysstat
     tmux
@@ -167,11 +168,11 @@ in
       config = ''
          set daemon 300 with start delay 120
          set mailserver mail.little-fluffy.cloud
-         set alert root@little-fluffy.cloud
+         set alert root@little-fluffy.cloud reminder on 10 cycles
          set eventqueue basedir /var/monit slots 5000
 
          check filesystem mail.little-fluffy.cloud-rootfs with path /dev/vda3
-                if space usage > 30% then alert
+                if space usage > 50% then alert
 
          check program SystemDegraded with path "/run/current-system/sw/bin/systemctl is-system-running"
            if status != 0 then alert
