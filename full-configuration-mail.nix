d05ae4de63b1197891f2f67487299db46956f1d3
Steve Sosik <ssosik@gmail.com>
2020-08-12 10:19:35 -0400

    Update reminder cycles to every 12 hours

--- full-configuration-mail.nix
+++ full-configuration-mail.nix
@@ -168,7 +168,7 @@ in
       config = ''
          set daemon 300 with start delay 120
          set mailserver mail.little-fluffy.cloud
-         set alert root@little-fluffy.cloud reminder on 10 cycles
+         set alert root@little-fluffy.cloud reminder on 120 cycles
          set eventqueue basedir /var/monit slots 5000
 
          check filesystem mail.little-fluffy.cloud-rootfs with path /dev/vda3
